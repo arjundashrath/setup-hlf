93e2dc8a6dec64e9fd283a7bbc947cba6300e450
arjundashrath <arjundashrath@gmail.com>
2022-09-16 12:11:34 +0530

    Download hlf data

--- setup.sh
+++ setup.sh
@@ -22,4 +22,6 @@ sudo systemctl start docker
 
 echo "Test Successful"
 
+curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
 
+./install-fabric.sh
