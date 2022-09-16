6b13e80ed4989fa6f9347c30c0253a981360f24b
arjundashrath <arjundashrath@gmail.com>
2022-09-16 13:00:40 +0530

    Launch setup-hlf action

--- setup.sh
+++ setup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-echo "Test Start"
+echo "Running setup-hlf github action..."
+
+echo "Checking prerequisites..."
+
+echo "Installing Git..."
+
 sudo apt-get install git
-
-echo "Installing Git"
-
 git --version
 
+echo "Installing cURL..."
 sudo apt-get install curl
-
-echo "Installed cURL"
-
 curl -V
 
+echo "Installing docker and docker-compose..."
 sudo apt-get -y install docker-compose
-
 docker --version
 docker-compose --version
 
+echo "Starting docker daemon..."
 sudo systemctl start docker
 
-echo "Test Successful"
+echo "Prerequisite step done!"
 
+echo "Fetching Hyperledger Fabric install script..."
 curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
 
 if [ $# -eq 0 ]
@@ -29,7 +31,9 @@ if [ $# -eq 0 ]
     ./install-fabric.sh
 else
     echo "./install-fabric.sh --fabric-version $1 binary"
-    ./install-fabric.sh --fabric-version $1 binary
-    
+    ./install-fabric.sh --fabric-version $1     
 fi
+
+echo "Successfully run action!"
+
 
