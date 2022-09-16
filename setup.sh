aa5b26dfb8e089e1d8792db73ce4d5782822ea57
arjundashrath <arjundashrath@gmail.com>
2022-09-16 12:05:50 +0530

    Add docker pre-requisite

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Test Start"
 sudo apt-get install git
 
 echo "Installing Git"
@@ -11,4 +12,14 @@ sudo apt-get install curl
 echo "Installed cURL"
 
 curl -V
+
+sudo apt-get -y install docker-compose
+
+docker --version
+docker-compose --version
+
+sudo systemctl start docker
+
+echo "Test Successful"
+
 
